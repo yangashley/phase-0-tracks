31737af8502ec41da16f9f65568a6e0059a41af2
Ashley Yang <ashley.minghui@gmail.com>
2016-10-30 17:43:27 -0500

    Add employee portal

---
+++ ruby/vampires.rb
@@ -0,0 +1,18 @@
+puts "Welcome to the New Employee Portal. What is your name?"
+user_name = gets.chomp
+
+puts "Hi, #{user_name}. How old are you?"
+age = gets.chomp
+
+puts "Great! What year were you born?"
+birth_year = gets.chomp
+
+puts "Thanks. Our company cafeteria serves garlic bread. Should we order some for you? (yes/no)"
+garlic_bread = gets.chomp
+
+puts "Please list any allergies you may have or enter N/A if you don't have any."
+allergies = gets.chomp
+
+puts "Sounds good. One final question: Would you like to enroll in the company’s health insurance? (yes/no)"
+health_insur = gets.chomp
+puts "Wonderful! Thanks for the information."
