31e3861c0ada70287db79ea4ddfe0984de89feaf
Ashley Yang <ashley.minghui@gmail.com>
2016-11-01 10:56:45 -0500

    Add pseudocode for client info program

---
+++ ruby/hashes.rb
@@ -0,0 +1,16 @@
+=begin
+
+-Create empty hash for storing user data
+-Create a method that stores user input and convert to appropriate data type
+	-Take converted data and store as values for keys
+-Print message to asks user to fill out form with client's info
+	-Prints "Please complete the form"
+-Print hash out to screen when all questions answered
+-Prints "Do you have any updates to the form? Type 'yes' if you have updates. If not, type 'none'."
+	-If yes, prints "Please update the form"
+		-Print individual questions for user to complete
+		-Call previously created method to take user updates and convert to appropriate data type
+		-Take converted data and store as values for keys
+	-If none, print hash and exit
+
+=end
