102d606a7b1d1f4a7db4cdd1409bbaaecdb3195d
Ashley Yang <ashley.minghui@gmail.com>
2016-11-01 10:59:28 -0500

    Update pseudocode and write client info program

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,16 +1,79 @@
 =begin
 
 -Create empty hash for storing user data
--Create a method that stores user input and convert to appropriate data type
+-Print questions one at a time for user to answer
+-Convert user input to appropriate data type
 	-Take converted data and store as values for keys
--Print message to asks user to fill out form with client's info
-	-Prints "Please complete the form"
 -Print hash out to screen when all questions answered
 -Prints "Do you have any updates to the form? Type 'yes' if you have updates. If not, type 'none'."
-	-If yes, prints "Please update the form"
+	-If yes, print questions one at a time for user to answer
 		-Print individual questions for user to complete
-		-Call previously created method to take user updates and convert to appropriate data type
+		-Convert user input to appropriate data type
 		-Take converted data and store as values for keys
 	-If none, print hash and exit
 
 =end
+
+
+client_info = {
+	client_name: "",
+	client_age: "",
+	children: "",
+	decor_theme: "",
+	num_rooms: "",
+	fave_color: "",
+	electrical_work: ""
+}
+
+puts "Client's name:"
+name = gets.chomp
+puts "Client's age:"
+age = gets.to_i
+puts "Number of children:"
+num_children = gets.to_i
+puts "Decor theme:"
+decor = gets.chomp
+puts "Number of rooms to design:"
+rooms = gets.to_i
+puts "Client's favorite color:"
+color = gets.chomp
+puts "Will electrical work be involved (true/false):"
+electrical = gets.chomp
+
+client_info[:client_name] = name
+client_info[:client_age] = age
+client_info[:children] = num_children
+client_info[:decor_theme] = decor
+client_info[:num_rooms] = rooms
+client_info [:fave_color] = color
+client_info [:electrical_work] = electrical
+
+puts client_info
+
+client_name_array = []
+client_name_array << name
+
+puts "Would you like to update any of the information? (yes/no)"
+update = gets.chomp
+
+if update == 'yes'
+	puts "Client's name:"
+	name = gets.chomp
+	puts "Client's age:"
+	age = gets.to_i
+	puts "Number of chidlren:"
+	num_children = gets.to_i
+	puts "Decor theme:"
+	decor = gets.chomp
+	puts "Number of rooms to design:"
+	rooms = gets.to_i
+	puts "Client's favorite color:"
+	color = gets.chomp
+	puts "Will electrical work be involved (true/false):"
+	electrical = gets.chomp
+	client_name_array << name
+elsif update == 'no'
+puts "#{name}'s information has been saved."
+end
+
+puts client_info
