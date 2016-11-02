34864ef5bbb15bb49a219d9287e3718d296f7485
Ashley Yang <ashley.minghui@gmail.com>
2016-11-02 12:47:27 -0500

    Add nested data structure

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,32 @@
+#A school with classroom full of desks full of supplies
+
+school = {
+	room_100: {
+		student_desk: "Ashley",
+		desk: [
+			"Books",
+			"Pencil Pouch",
+			"Pencil Sharpener",
+			"Box of Crayons"
+		]
+	},
+	room_200: {
+		student_desk: "Colin",
+		desk: [
+			"Books",
+			"Eraser",
+			"Ruler",
+			"Box of Markers"
+		]
+	},
+	room_300: {
+		student_desk: "Meghann",
+		desk: [
+			"Books",
+			"Protractor",
+			"Pens",
+			"Sticky Notes"
+		]
+	}
+
+}
