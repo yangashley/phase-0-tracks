1b98b527bb31019280836221a2431a9202bfdebb
Ashley Yang <ashley.minghui@gmail.com>
2016-11-02 17:37:14 -0500

    Add outline of pseudocode

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,4 +1,25 @@
 =begin
+Alias Manager
 
-	
-end
+Method to convert inputted string into array
+
+Method to change a vowel to the next vowel
+-If, elsif, else condition for edge cases
+	-u should be a
+
+
+Method to change a consonant to the next consonant
+-If, elsif, else condition for edge cases
+-Call .downcase on the character that will take .next so that it will change capital letters
+	-z should be b
+
+Method to swap first and last name:
+-Set inputted first name and last name to an array
+-Switch the position of first name and last name in the array
+
+User Interface:
+-Ask user for first name and last name
+
+Store the Alias:
+-Feed the fake names into a new array
+=end
