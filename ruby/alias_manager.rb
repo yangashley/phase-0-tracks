ed98ff5c277f64f0dd315ef4233c5b6af7313e18
Ashley Yang <ashley.minghui@gmail.com>
2016-11-03 09:51:41 -0500

    Take name, reverse first and last, move letters next, fix vowels

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -23,3 +23,44 @@ User Interface:
 Store the Alias:
 -Feed the fake names into a new array
 =end
+
+
+
+user_name = [ ]
+first_name = "Ashley"
+last_name = "Yang"
+
+#can also split LastnameFirstname into Lastname Firstname at the capital letter 
+#only if it's a string using .split(/(?=[A-Z])/).join(' ')
+
+reverse_name = "#{last_name} #{first_name}"
+#Convert reversed name into an array of characters
+reverse_name.scan(/./)
+#Puts array of characters into a new array
+user_name << reverse_name.scan(/./)
+p user_name
+
+#you cannot run .next on an array. so you made a string an arary
+#you'll have to make the array back into the string to call .next
+change_name = user_name.join
+
+#new variable holding reversed name as a string to call .next
+p change_name
+
+
+
+def change_letter(name)
+	index = 0
+	while index < name.length
+	if name[index] == "z"
+  	name[index] = "a"
+  	elsif name[index] == " "
+  	else
+  	name[index] = name[index].next!
+  end
+    index += 1
+ end
+ puts "#{name}"
+end
+
+puts change_letter(change_name)
