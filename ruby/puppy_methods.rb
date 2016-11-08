e2178a4d3d4de63f466d704ad196224e1eaeecee
Ashley Yang <ashley.minghui@gmail.com>
2016-11-08 13:15:07 -0600

    Add puppy behavior

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,33 @@ class Puppy
     toy
   end
 
+  def speak(integer)
+  	integer.times {puts "Woof"}
+  end
+
+  def roll_over
+  	puts "*rolls over*"
+  end
+
+  def dog_years(integer)
+  	dog_years = integer * 7
+  	p dog_years
+  end
+
+  def sit
+  	puts "*sits*"
+  end
+
+  def initialize
+  	
+  end
+
 end
 
+#Driver Code
+meatball = Puppy.new
+meatball.fetch("ball")
+meatball.speak(4)
+meatball.roll_over
+meatball.dog_years(3)
+meatball.sit
