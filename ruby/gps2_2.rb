3fcbf7f7efefe442467c8afbb67a21db40f60b41
Ashley Yang <ashley.minghui@gmail.com>
2016-11-08 11:20:25 -0600

    Update quantity method

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -1,6 +1,6 @@
-	# Method to create a list
+# Method to create a list
 # input: string of items separated by spaces (example: "carrots apples cereal pizza")
-   #Break apart string of items on the space
+ #Break apart string of items on the space
 # steps: 
 # Create an empty item array to hold items for list
 # Call each on the item array to turn each value into a key for the hash
@@ -27,14 +27,16 @@
 # Create a method that takes two parameters (hash and key)
 # Check to see if item to be deleted is already in the list 
 # Iterate through the hash to see if the item already exists 
-	# IF the item exists, delete it
-	# IF item doesn't exist, return a message that says "Item not found"
+   # IF the item exists, delete it
+   # IF item doesn't exist, return a message that says "Item not found"
 # output: revised list with item deleted
 
 # Method to update the quantity of an item
-# input:
-# steps:
-# output:
+# input: You passing in the shopping list, the item you want to update and the quantity. You are passing in a hash, a string and an integer.
+# steps: We are going to create a method called update_quantity.
+# Overide the value of the key by setting it equal to the new quantity. 
+# IF item does not exist in the list don't add the item. We will create an if statement and the key is nil we won't update the hash. 
+# output: The updated list with the new quantity.
 
 # Method to print a list and make it look pretty
 # input:
@@ -42,29 +44,36 @@
 # output:
 
 def add_items(inputted_list) 
-      list = {}
-   list_array = inputted_list.split(' ')
-   
-   list_array.each do |item|
-       list[item] = 1
+    list = {}
+ list_array = inputted_list.split(' ')
+ 
+ list_array.each do |item|
+     list[item] = 1
+ end
+ list
+end
+
+def new_item(list, item, quantity=1)
+  if list[item] == nil
+      list[item] = quantity 
+  end
+  list
+end
+
+def delete_item(list, delete_item)
+   list.each do |item, quantity|
+       if list[item] == list[delete_item]
+           list.delete(item)
+       end
    end
    list
 end
 
-def new_item(list, item, quantity=1)
-    if list[item] == nil
-        list[item] = quantity 
+def update_quantity(list, item, quantity)
+    if list[item] != nil
+        list[item] = quantity
     end
     list
-end
-
-def delete_item(list, delete_item)
-	list.each do |item, quantity|
-		if list[item] == list[delete_item]
-			list.delete(item)
-		end
-	end
-	list
 end
 
 inputted_list = "bananas tomatoes apples bread"
@@ -73,7 +82,8 @@ list = add_items(inputted_list)
 new_item(list, "bananas", 5)
 p list
 delete_item(list, "bananas")
+update_quantity(list, "pizzas", 5)
+update_quantity(list, "tomatoes", 15)
 p list
-
 
 
