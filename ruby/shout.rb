37929d8314b761a1b7f617e247653114737ac5b3
Ashley Yang <ashley.minghui@gmail.com>
2016-11-08 17:58:09 -0600

    Write shout module with angry and happy yelling

---
+++ ruby/shout.rb
@@ -0,0 +1,13 @@
+module Shout
+def self.yell_angrily(words)
+    words + "!!!" + " :("
+  end
+
+ def self.yell_happily(words)
+ 	words + "!!!!!!! :)"
+ end
+end
+
+#Driver code
+Shout.yell_angrily("Why didnt you unload the dishwasher?")
+Shout.yell_happily("Thanks for cleaning the bathroom")
