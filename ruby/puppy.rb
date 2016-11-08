63b51e6b004af6d8ad7f8199694dd0bff047f86e
Ashley Yang <ashley.minghui@gmail.com>
2016-11-07 18:27:47 -0600

    Add puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,23 @@
+=begin
+Class
+
+SPECIES ------------------------
+Puppy
+
+CHARACTERISTICS ----------------
+Name: varies
+Friendly: yes
+Fur color: varies
+Coat length: varies
+Playful: yes
+Lazy: yes
+Naughty: varies
+
+BEHAVIOR -----------------------
+Run
+Walk 
+Bark
+Lick
+Sleep
+Play
+=end
