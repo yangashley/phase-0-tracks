09eef81ba00de6238d2a499e6ddddc7423b09958
Ashley Yang <ashley.minghui@gmail.com>
2016-11-08 18:23:45 -0600

    Create fan and children class, convert module to mixin

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,14 +1,44 @@
 module Shout
-def self.yell_angrily(words)
-    words + "!!!" + " :("
-  end
+	def yell_happily(words)
+ 		puts words + "!!!!!!! :)"
+ 	end
 
- def self.yell_happily(words)
- 	words + "!!!!!!! :)"
- end
+ 	def yell_angrily(words)
+    	puts words + "!!!" + " :("
+ 	end
 end
 
-#Driver code
-Shout.yell_angrily("Why didnt you unload the dishwasher?")
-Shout.yell_happily("Thanks for cleaning the bathroom")
+class SportsFans
+	include Shout
+end
+
+class Children
+	include Shout
+end
+
+fan = SportsFans.new
+fan.yell_happily("We won")
+fan.yell_angrily("We lost")
+
+kid = Children.new
+kid.yell_happily("Time for recess")
+kid.yell_angrily("Recess is over")
+
+
+
+
+
+# module Shout
+# def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+
+#  def self.yell_happily(words)
+#  	words + "!!!!!!! :)"
+#  end
+# end
+
+# #Driver code
+# Shout.yell_angrily("Why didnt you unload the dishwasher?")
+# Shout.yell_happily("Thanks for cleaning the bathroom")
 
