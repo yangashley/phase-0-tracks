603c04b84724713e6375428c97ffd15b9ea156e5
Ashley Yang <ashley.minghui@gmail.com>
2016-11-11 09:51:46 -0600

    Add pseudocode for game

---
+++ ruby/6.6_game/game.rb
@@ -0,0 +1,34 @@
+=begin
+Pseudocode 
+
+-Create a class called Game
+There will need to be a way to use the guess(a word) with the different methods
+
+Method 1: Initialize instances of the game class
+-input: A secret word that is a string (example: 'hello')
+-steps:
+	-The class will have attributes with values of the secret word, number of guesses, state of the game (whether it's over or not)
+-output: Print out the message 'Starting game...'
+
+Method 2: Turn a guess into an array of characters
+-input: A guess that is a string (example 'where')
+-steps: 
+	-Turn guess into an array of characters and set it equal to a variable so it can be used in other methods
+-output: an array (example player_guess = ['w', 'h', 'e', 'r', ''])
+
+Method 3: Check guesses against secret word (example player_guess = ['w', 'h', 'e', 'r', ''])
+-input: the guess as an array of characters 
+-steps:
+	-Create an empty feedback array to store letters correctly guessed 
+	-Iterate through each character in the guess array to see if it is in the secret word string
+		-IF the character is in the secret word, it will be stored in the feedback array as itself
+		-IF the character is not in the secret word, store '- ' instead
+	-Return the array 
+-output: feedback = ['w', _ _ 'r', _])
+
+User Interface: 
+-The user has as many chances to guess as there are letters in the word (5 letters in secret word = 5 chances to guess)
+-While the game is not over, ask for a guess
+	-IF all guesses are used, game is over
+	-IF secret word is correctly guessed, game is over
+=end
