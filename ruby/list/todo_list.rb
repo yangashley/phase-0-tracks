ce1bb347fef613c0e543b3b36fc7131526477c11
Ashley Yang <ashley.minghui@gmail.com>
2016-11-10 19:03:22 -0600

    Add item, delete item, get item methods to pass tests

--- ruby/list/todo_list.rb
+++ ruby/list/todo_list.rb
@@ -8,4 +8,15 @@ class TodoList
 		@todo_list
 	end
 
+	def add_item(new_item)
+		@todo_list << new_item
+	end
+
+	def delete_item(delete_chore)
+		@todo_list.delete(delete_chore)
+	end
+
+	def get_item(list_index)
+		@todo_list[list_index]
+	end
 end
