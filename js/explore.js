75a275b421ca454ee2c6fa73f3a3dee049c837d4
Ashley Yang <ashley.minghui@gmail.com>
2016-11-15 09:10:47 -0600

    Add method to reverse string

--- js/explore.js
+++ js/explore.js
@@ -3,4 +3,17 @@
 // Reverse the characters in the array
 // Join the reversed characters back together into a string
 
- 
+function reverseString(string) {
+
+var splitString = string.split("");
+var reverseArray = splitString.reverse();
+var joinString = reverseArray.join("");
+return joinString;
+}
+
+reversed_string = reverseString("hello");
+
+if (1==1) {
+	console.log(reversed_string);
+}
+
