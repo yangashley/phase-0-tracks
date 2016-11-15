afaa13bf6c3a87213d89b13cae2a565284a14f8c
Ashley Yang <ashley.minghui@gmail.com>
2016-11-15 08:58:53 -0600

    Add pseudocode for reverse method

---
+++ js/explore.js
@@ -0,0 +1,6 @@
+// Pseudocode to create a method that takes a string and reverses it
+// Turn a string into an array 
+// Reverse the characters in the array
+// Join the reversed characters back together into a string
+
+ 
