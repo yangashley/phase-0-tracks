155700fb27dea4cf5b7d49c96f8274a6f90bef2c
Ashley Yang <ashley.minghui@gmail.com>
2016-11-16 15:40:16 -0600

    Add constructor function

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,14 +1,46 @@
-colors = ["brown", "grey", "black", "white"];
-names = ["Ed", "Sunshine", "Nightmare", "Thunder"];
+// colors = ["brown", "grey", "black", "white"];
+// names = ["Ed", "Sunshine", "Nightmare", "Thunder"];
 
-colors.push("tan");
-names.push("Bob");
+// colors.push("tan");
+// names.push("Bob");
 
-//create an empty object, horses
-//use a for loop to iterate through names array, each item becomes a key
-//use another for loop to iterate through the colors array, each item becomes a value
-//horses['Ed'] = "brown";
+// //create an empty object, horses
+// //use a for loop to iterate through names array, each item becomes a key
+// //use another for loop to iterate through the colors array, each item becomes a value
+// //horses['Ed'] = "brown";
 
-var horses = {};
-for (var i = 0; i < names.length; i++)
-	horses[names[i]] = colors[i];
+// var horses = {};
+// for (var i = 0; i < names.length; i++){
+// 	horses[names[i]] = colors[i];
+// }
+
+// console.log(horses);
+
+function Car(used, year, color) {
+	this.used = used;
+	this.year = year;
+	this.color = color;
+
+	this.race = function() {console.log("Vroom!");};
+
+	this.print = function() {
+		if(used == true){
+			console.log("This is a used car");
+		} 
+		else if(used == false){
+			console.log("This is a new car");
+		}
+		console.log("Year: " + year);
+		console.log("Color: " + color);
+	};
+	this.print();
+	console.log("Car initialization complete!");
+	}
+
+var jeep = new Car(false, 2015, "black");
+var subaru = new Car(true, 2000, "green");
+var mazda = new Car(false, 2016, "silver");
+
+jeep.race();
+subaru.race();
+mazda.race();
