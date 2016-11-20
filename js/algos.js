bfc3c3487b12a7ce02eeac4067887f49a24f08f0
Ashley Yang <ashley.minghui@gmail.com>
2016-11-20 10:57:40 -0600

    Add function to check if there is at least one matching pair in two objects

--- js/algos.js
+++ js/algos.js
@@ -1,13 +1,59 @@
-// Write a function that takes an array of words or phrases and returns the longest word or phrase in the array. So if we gave your function the array of ["long phrase","longest phrase","longer phrase"], it would return "longest phrase". This is a logical reasoning challenge, so try to reason through this based on what you already know, rather than Googling the fanciest solution or looking up built-in sorting functions. Note that "some string".length will do what you expect in JS.
-//Add driver code that tests your function on a few arrays.
-
 //Pseudocode 
-//Create a variable equal to 0 called phrase_length
-//Iterate through each phrase in the array to check for the length of the phrase
-//Store the phrase and length of each phrase as keys and values in an object
-//Iterate through the object 
-	//IF the length of the phrase is greater than the value phrase_length, set the variable to equal the length of the phrase
-//After iteration when the longest length has been returned, use the phrase length (value) to find the phrase (key).
-//Print the key to show which phrase is the longest.
+
+//Input: Array of phrases
+//Steps:
+	//Create a variable equal to the first phrase in the array called phrase_length
+	//Iterate through each phrase in the array to check for the length of the phrase
+		//IF the length of the phrase is greater than the value phrase_length, set the variable to equal the length of the phrase
+	//Return the value of the longest phrase in the array
+//Output: longest phrase in array
+
+// an array with phrases
+
+function longest_phrase(array) {
+	longest_length = array[0]
+	for (var i = 0; i < array.length; i++) {
+		if (array[i].length > longest_length.length) {
+			(longest_length = array[i]);
+		}
+	}
+	return longest_length;
+};
+
+// A function that takes two objects and checks to see if the objects share at least one key-value pair.
+//Input: 2 Objects
+//Steps:
+	//Compare the pairs from each object. 
+		//IF the pairs are the same, return true.
+		//IF the pairs are not the same, return false.
+//Output: boolean (true if there is a match, false if there is not a match).
+
+function match_pairs(object_1, object_2){
+
+	if(object_1.name == object_2.name){
+		return true;
+	} else if (object_1.age == object_2.age){
+		return true;
+	} else {
+		return false;
+	}
+}
+
+
+// driver code for longest_phrase function
+var array = new Array("long phrase", "even longer phrase", "longer phrase", "longest phrase");
+
+console.log(longest_phrase(array));
+
+// driver code for match_pairs function
+var object_1 = {name: 'Ashley', age: 25};
+
+var object_2 = {name: 'Colin', age: 25};
+
+var object_3 = {name: 'Marisa', age: 24};
+
+console.log(match_pairs(object_1, object_2));
+console.log(match_pairs(object_2, object_3));
+
 
 
