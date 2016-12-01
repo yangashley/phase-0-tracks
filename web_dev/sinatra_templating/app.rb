b429e646ac9ad108864948eaf15e5633bc076835
Ashley Yang <ashley.minghui@gmail.com>
2016-12-01 09:53:13 -0600

    Create a route and add a form

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -1,3 +1,4 @@
+
 # require gems
 require 'sinatra'
 require 'sqlite3'
@@ -7,7 +8,11 @@ set :public_folder, File.dirname(__FILE__) + '/static'
 db = SQLite3::Database.new("students.db")
 db.results_as_hash = true
 
+db_2 = SQLite3::Database.new("people.db")
+db_2.results_as_hash = true
+
 # show students on the home page
+# / is localhost:9393
 get '/' do
   @students = db.execute("SELECT * FROM students")
   erb :home
@@ -25,3 +30,19 @@ post '/students' do
 end
 
 # add static resources
+# create a new (url) route to go to practice page 
+# show all people in the people.db
+get '/practice' do
+	@persons = db_2.execute("SELECT * FROM persons")
+	erb :practice
+end
+
+# takes you to page to add new person
+get '/people/new' do
+	erb :search_results
+end
+
+post '/people' do
+	db_2.execute("INSERT INTO persons (name, age, gender) VALUES (?, ?, ?)", [params['name'], params['age'].to_i, params['gender']])
+	redirect '/practice'
+end
